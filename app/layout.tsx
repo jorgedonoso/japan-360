9d7b0d04ee7061f8e871e569391b966d897e826d
Jorge Donoso <jdevcl@gmail.com>
2026-04-21 12:45:56 -0700

    Add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "@photo-sphere-viewer/core/index.css";
 import PrefectureSelect from "@/components/PrefectureSelect";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body>
+        <Analytics></Analytics>
         <PrefectureSelect></PrefectureSelect>
         {children}
       </body>
