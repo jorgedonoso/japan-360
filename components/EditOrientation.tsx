29712d38b29e5af7ec82a0182dd138423d0f9daa
Jorge Donoso <jdevcl@gmail.com>
2026-06-11 12:00:29 -0700

    Add Clear Local Storage feature

--- components/EditOrientation.tsx
+++ components/EditOrientation.tsx
@@ -6,13 +6,18 @@ import PrefectureSelect from "./PrefectureSelect";
 import { toast } from "react-toastify";
 import { isProduction } from "@/src/util/helpers";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSave } from "@fortawesome/free-solid-svg-icons";
+import { faSave, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function EditOrientation() {
   const prefecture = useLocationStore((s) => s.selectedPrefecture);
   const yaw = useLocationStore((s) => s.objects[prefecture!]?.yaw);
   const pitch = useLocationStore((s) => s.objects[prefecture!]?.pitch);
   const isViewerReady = useLocationStore((s) => s.isViewerReady);
+
+  const handleClearLocalStorage = () => {
+    localStorage.clear();
+    toast.success("Local Storage Cleared");
+  };
 
   const handleSave = async () => {
     if (isProduction()) {
@@ -39,7 +44,7 @@ export default function EditOrientation() {
     <div className="absolute w-64 m-2 rounded text-black z-[9999]">
       <div className="max-w-md mx-auto p-4 bg-white rounded-lg shadow">
         <PrefectureSelect></PrefectureSelect>
-        <form className="space-y-4">
+        <form className="space-y-2">
           <div>
             <label className="block font-bold mb-1">Yaw:</label>
             {yaw}
@@ -56,6 +61,16 @@ export default function EditOrientation() {
             <FontAwesomeIcon icon={faSave} className="mr-1" />
             Save Orientation
           </button>
+          {isProduction() && (
+            <button
+              type="button"
+              onClick={handleClearLocalStorage}
+              className="cursor-pointer font-bold w-full bg-orange-500 text-white py-2 px-4 rounded-md hover:bg-orange-600 transition"
+            >
+              <FontAwesomeIcon icon={faTrash} className="mr-1" />
+              Clear Local Storage
+            </button>
+          )}
         </form>
       </div>
     </div>
