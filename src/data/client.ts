b1a6b7a6dc16ac437436daf4b756e7f13106fa13
Jorge Donoso <jdevcl@gmail.com>
2026-06-17 12:01:04 -0700

    Fix db path

--- src/data/client.ts
+++ src/data/client.ts
@@ -1,7 +1,7 @@
 import Database from "better-sqlite3";
 import path from "path";
 
-const dbPath = path.join(process.cwd(), "/src/data/app.db");
+const dbPath = path.join(process.cwd(), "src/data/app.db");
 const db = new Database(dbPath);
 
 export default db;
